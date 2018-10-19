6bc2352f975e0275b969812531dcc2f5abb42438
wurongqing <2213629594@qq.com>
2018-10-19 14:41:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿##   一个简单c# Winform 翻译软件
+
+*  要去百度翻译API 网站 注册一个账号 获取你的 appid 和 秘钥  开通百度翻译 API 接口翻译
+
+  [百度翻译API]: http://api.fanyi.baidu.com/api/trans/product/index
+
+* 在  AppConfig 中 配置你 申请到 APPID  和  秘钥 ，可以直接运行项目查看效果
+
+* 效果图：
+
+ ![](C:\Users\Administrator\Desktop\QQ图片20181019143558.png)
+
+* 可以直接在框中 输入你需要翻译的中文 不用点击翻译按钮， 当然如果你想点击也可以
+
+  毕竟功能是有的。
---
+++ TranslateDemo/frmtranslate.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TranslateDemo
+{
+    partial class frmtranslate
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.btntranslate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(24, 43);
+            this.txtFrom.Multiline = true;
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(483, 112);
+            this.txtFrom.TabIndex = 0;
+            // 
+            // btntranslate
+            // 
+            this.btntranslate.Location = new System.Drawing.Point(267, 171);
+            this.btntranslate.Name = "btntranslate";
+            this.btntranslate.Size = new System.Drawing.Size(117, 34);
+            this.btntranslate.TabIndex = 1;
+            this.btntranslate.Text = "翻译";
+            this.btntranslate.UseVisualStyleBackColor = true;
+            this.btntranslate.Click += new System.EventHandler(this.btntranslate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(390, 171);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 34);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "清空";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(24, 226);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(483, 112);
+            this.txtTo.TabIndex = 1;
+            // 
+            // frmtranslate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 364);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btntranslate);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Name = "frmtranslate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "属于自己的翻译";
+            this.Load += new System.EventHandler(this.frmtranslate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btntranslate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtTo;
+    }
+}
+
